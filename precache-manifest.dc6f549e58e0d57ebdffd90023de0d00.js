638e10641b0bfec57bee5ac25f4ab2af3a468189
Sam Kendrick <sam.kendrick@powerreviews.com>
2020-07-22 09:48:36 -0500

    Updates

---
+++ precache-manifest.dc6f549e58e0d57ebdffd90023de0d00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2afbffcc67707a84ebbe051e04d50d9e",
+    "url": "/@adnauseum/use-browser-cache/index.html"
+  },
+  {
+    "revision": "effb3abc5e714974ed87",
+    "url": "/@adnauseum/use-browser-cache/static/css/main.09608258.chunk.css"
+  },
+  {
+    "revision": "9fe7aed849b22b347404",
+    "url": "/@adnauseum/use-browser-cache/static/js/2.bc5b1747.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/@adnauseum/use-browser-cache/static/js/2.bc5b1747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "effb3abc5e714974ed87",
+    "url": "/@adnauseum/use-browser-cache/static/js/main.a6a506e3.chunk.js"
+  },
+  {
+    "revision": "8bf067631d520efc422a94c742d0ad1d",
+    "url": "/@adnauseum/use-browser-cache/static/js/main.a6a506e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd573f16320185c355ef",
+    "url": "/@adnauseum/use-browser-cache/static/js/runtime-main.25089709.js"
+  }
+]);
